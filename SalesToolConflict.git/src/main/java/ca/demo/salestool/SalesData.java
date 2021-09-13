ead9ae266324db1756449c5378f0f120140f9e71
Rimma77 <rimma34@gmail.com>
2021-09-13 22:20:42 +0200

    master/ add 2 classes

---
+++ SalesToolConflict.git/src/main/java/ca/demo/salestool/SalesData.java
@@ -0,0 +1,12 @@
+package ca.demo.salestool;
+
+
+public class SalesData {
+	private int data[] = {0, 4, 42};
+
+	public void display() {
+		for (int i = 0; i < data.length; i++) {
+			System.out.println("next value: " + data[i]);
+		}
+	}
+}
